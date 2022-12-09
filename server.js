872d3e9c8f7efb83c4f27fdd044796a9194be16c
Bao-Nhi Vu <69761376+baonhivu@users.noreply.github.com>
2022-12-08 19:12:23 -0500

    Update server.js

--- server.js
+++ server.js
@@ -84,7 +84,7 @@ app.post('/loginpage', function(req, res){
 
 //Route to homepage
 app.get('/home', function(req, res){
-    const datastmt = db.prepare(`SELECT watergoal FROM userinfo WHERE username = '${req.app.get('username')}';`);
+    const datastmt = db.prepare(`SELECT * FROM userinfo WHERE username = '${req.app.get('username')}';`);
     let wgoal = datastmt.all();
     res.render('home', {'userinfo' : wgoal});
 });
